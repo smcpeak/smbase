289a3071c4ad16b4ebf5acb804f19d17eea9f014
Scott McPeak <smcpeak@acm.org>
2009-08-09 14:45:36 -0700

    added sm-iostream.h

---
+++ sm-iostream.h
@@ -0,0 +1,18 @@
+// sm-iostream.h
+// iostream.h stuff
+
+#ifndef SM_IOSTREAM_H
+#define SM_IOSTREAM_H
+
+#include <iostream>                    // std:: stuff
+
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::clog;
+using std::endl;
+using std::iostream;
+using std::ostream;
+using std::istream;
+
+#endif // SM_IOSTREAM_H
