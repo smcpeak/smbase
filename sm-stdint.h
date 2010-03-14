da13c4c7690a637bb949f953c67cf4cf1097e01e
Scott McPeak <smcpeak@acm.org>
2010-03-13 16:30:28 -0800

    created sm-stdint.h

---
+++ sm-stdint.h
@@ -0,0 +1,14 @@
+// sm-stdint.h
+// wrapper around stdint.h
+
+#ifndef SM_STDINT_H
+#define SM_STDINT_H
+                   
+// for now, just assume it exists
+#include <stdint.h>
+
+// symbols we want to export:
+// * intptr_t
+// * uintptr_t
+
+#endif // SM_STDINT_H
