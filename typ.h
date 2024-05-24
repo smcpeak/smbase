c1a1dcb9e2625b862fcaf6207c2aa63c1fb21fc2
Scott McPeak <scott.g.mcpeak@gmail.com>
2024-05-24 13:11:06 -0700

    typ.h: Move `div_up` into new div-up.h.

--- typ.h
+++ typ.h
@@ -14,33 +14,10 @@
 #include <stddef.h>                    // NULL
 
 
-// division with rounding towards +inf
-// (when operands are positive)
-template <class T>
-inline T div_up(T const &x, T const &y)
-{ return (x + y - 1) / y; }
-
-
-// mutable
-#ifdef __BORLANDC__
-#  define MUTABLE
-#  define EXPLICIT
-#else
-#  define MUTABLE mutable
-#  define EXPLICIT explicit
-#endif
-
-
 #define SWAP(a,b) \
   temp = a;       \
   a = b;          \
   b = temp /*user supplies semicolon*/
-
-
-// verify something is true at compile time (will produce
-// a compile error if it isn't)
-// update: use STATIC_ASSERT defined in macros.h instead
-//#define staticAssert(cond) extern int dummyArray[cond? 1 : 0]
 
 
 #endif // SMBASE_TYP_H
