35bc10a478d35432875f033befa2bb5f7848cace
Scott McPeak <scott.g.mcpeak@gmail.com>
2024-06-01 16:20:31 -0700

    gdvsymbol-test: Add tests to get coverage for gdvsymbol.cc to 100%.

--- gdvsymbol-test.cc
+++ gdvsymbol-test.cc
@@ -17,14 +17,24 @@ void test_gdvsymbol()
 {
   GDVSymbol s1;
   EXPECT_EQ(s1.getSymbolName(), "null");
+  EXPECT_EQ(s1.size(), 4);
 
   GDVSymbol s2("hello");
   EXPECT_EQ(s2.getSymbolName(), "hello");
+  EXPECT_EQ(s2.size(), 5);
 
   s1.swap(s2);
 
   EXPECT_EQ(s2.getSymbolName(), "null");
   EXPECT_EQ(s1.getSymbolName(), "hello");
+
+  xassert(!GDVSymbol::validSymbolName(""));
+  xassert(GDVSymbol::validSymbolName("_"));
+  xassert(GDVSymbol::validSymbolName("_9"));
+  xassert(!GDVSymbol::validSymbolName("9"));
+  xassert(GDVSymbol::validSymbolName("a9"));
+  xassert(!GDVSymbol::validSymbolName("a!"));
+  xassert(!GDVSymbol::validSymbolName("!"));
 }
 
 
