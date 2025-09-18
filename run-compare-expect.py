364fd8aaba5b2eebd297af888c37618d02321c59
Scott McPeak <scott.g.mcpeak@gmail.com>
2025-09-18 06:33:07 -0700

    run-compare-expect.py: Add "from boilerplate import *".

--- run-compare-expect.py
+++ run-compare-expect.py
@@ -26,57 +26,9 @@ import sys                   # sys.argv, sys.stderr
 import time                  # time.sleep
 import traceback             # traceback.print_exc
 
+from boilerplate import *
 from typing import Any, Match, Pattern, TextIO
 
-
-# -------------- BEGIN: boilerplate -------------
-# These are things I add at the start of every Python program to
-# allow better error reporting.
-
-# Positive if debug is enabled, with higher values enabling more printing.
-debugLevel = 0
-if debugEnvVal := os.getenv("DEBUG"):
-  debugLevel = int(debugEnvVal)
-
-def debugPrint(str: str) -> None:
-  """Debug printout when DEBUG >= 2."""
-  if debugLevel >= 2:
-    print(str)
-
-# Ctrl-C: interrupt the interpreter instead of raising an exception.
-signal.signal(signal.SIGINT, signal.SIG_DFL)
-
-class Error(Exception):
-  """A condition to be treated as an error."""
-  pass
-
-def die(message: str) -> None:
-  """Throw a fatal Error with message."""
-  raise Error(message)
-
-def exceptionMessage(e: BaseException) -> str:
-  """Turn exception 'e' into a human-readable message."""
-  t = type(e).__name__
-  s = str(e)
-  if s:
-    return f"{t}: {s}"
-  else:
-    return f"{t}"
-
-def call_main() -> None:
-  """Call main() and catch exceptions."""
-  try:
-    main()
-
-  except SystemExit as e:
-    raise      # Let this one go, otherwise sys.exit gets "caught".
-
-  except BaseException as e:
-    print(f"{exceptionMessage(e)}", file=sys.stderr)
-    if (debugLevel >= 1):
-      traceback.print_exc(file=sys.stderr)
-    sys.exit(2)
-# --------------- END: boilerplate --------------
 
 def splitLines(data: bytes) -> list[str]:
   """Decode byte list 'data' to text, split it at line boundaries
@@ -382,8 +334,9 @@ def main() -> None:
       sys.exit(2)
 
 
-testAssertionFailedReplacer()
-call_main()
+if __name__ == "__main__":
+  testAssertionFailedReplacer()
+  call_main(main)
 
 
 # EOF
